f3a4860faf561f01e6eb37987531554e92eb2158
Grenky <demosoft62@gmail.com>
2023-12-06 17:50:35 +0200

    3

--- src/ReduxTest.jsx
+++ src/ReduxTest.jsx
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'; 
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 
 export default function ReduxTest() {
 
 
     const dispatch = useDispatch()
-    const cash = useSelector(state => state.cash)
+    const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
 
 
     const addCash = (cash) => {
@@ -18,12 +20,39 @@ export default function ReduxTest() {
         };
     }
 
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now(),
+        }
+        dispatch(addCustomerAction(customer))
+    }
+
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer.id))
+    }
+
     return(
         <div className="reduxtest-wrapper">
             <div>
                 <div>{cash}</div>
                 <button onClick={() => addCash(Number(prompt()))}>add cash</button>
                 <button onClick={() => getCash(Number(prompt()))}>get cash</button>
+                <button onClick={() => addCustomer(prompt())}>add client</button>
+                <button onClick={() => removeCustomer(prompt())}>remove client</button>
+            </div>
+            <div>
+                {customers.length > 0 ? 
+                <div>
+                    {customers.map(customer => 
+                        <div onClick={() => removeCustomer(customer)}>{customer.name}</div>
+                    )}
+                </div>
+                :
+                <div>
+                    Clients none!
+                </div>
+                }
             </div>
         </div>
     )
